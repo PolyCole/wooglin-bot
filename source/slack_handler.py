f7e5ce8749cd2c2000383c290292d9a9b4cf7931
cole polyak <colepolyak@gmail.com>
2021-02-24 15:56:42 -0700

    Added test cases for the methods in slack_handler.

--- source/slack_handler.py
+++ source/slack_handler.py
@@ -22,8 +22,7 @@ def slack_handler(event, context):
 
     # Verifying that our requests are actually coming from slack.
     if "token" in event_body:
-        token = os.environ["SLACK_VERIFICATION_TOKEN"].split(",")
-        if event_body['token'] != token[0] and event_body['token'] != token[1]:
+        if event_body['token'] != os.environ["SLACK_VERIFICATION_TOKEN"]:
             print("VERIFICATION FOR SLACK FAILED.")
             print("This request didn't come from slack, telling sender to kindly go away.")
             return get_response_object(event, "Events originating outside of slack are not allowed.", 401)
@@ -36,7 +35,6 @@ def slack_handler(event, context):
     if "challenge" in event_body:
         response = get_response_object(event, "Challenge received, returning...", 200)
         response["challenge"] = event_body["challenge"]
-        print("Responding to slack challenge with: " + json.dumps(response))
         return response
 
     # Getting the slack data of the event.
@@ -55,53 +53,55 @@ def slack_handler(event, context):
 
         # If we're dealing with a DM channel, let's respond. Otherwise, let's ensure we're being @'ed.
         if 'channel_type' in slack_event and slack_event['channel_type'] == "im":
-            predefined_message_check(text, slack_event)
+            return get_response_object(event, predefined_message_check(text, slack_event), statusCode=200)
         else:
             if mentions_me(text):
-                predefined_message_check(strip_text(text), slack_event)
+                return get_response_object(event, predefined_message_check(strip_text(text), slack_event), statusCode=200)
 
     return get_response_object(event)
 
 
+# TODO: This whole flow needs refactoring.
 def predefined_message_check(text, slack_event):
     # In all honesty, it's probably better to wake the NLU for this, but that's an expensive op and
     # I'm not sure I have the budget for it.
     # TODO: Revisit this section with the power of regex or something and clean it up.
     secret_prompt_message_variations = [
         os.environ['SECRET_PROMPT'],
-        "Wooglin, " + os.environ['SECRET_PROMPT'],
-        "Wooglin " + os.environ['SECRET_PROMPT']
+        "wooglin " + os.environ['SECRET_PROMPT'],
+        ", friendship"
     ]
 
     funkytown_message_variations = [
-        'Wooglin, play funkytown',
-        'Wooglin play funkytown',
         'play funkytown',
         'play funky town',
-        'Wooglin, play funky town'
+        'wooglin, play funky town',
+        'play funkytown wooglin',
+        'play funky town wooglin'
     ]
 
     try:
         # Some classics.
         if text in secret_prompt_message_variations:
             print("Message contained secret prompt, returning secret response.")
-            sendmessage(os.environ['SECRET_RESPONSE'])
+            return sendmessage(os.environ['SECRET_RESPONSE'])
         elif text in funkytown_message_variations:
             print("Message requested funkytown.")
-            sendmessage("https://www.youtube.com/watch?v=s36eQwgPNSE")
+            return sendmessage("https://www.youtube.com/watch?v=s36eQwgPNSE")
         # Not a given response, let's send the message to NLP.
         else:
             print("Un-implemented.")
             # This gets more complicated.
             # process_message(slack_event)
     except Exception as e:
-        sendmessage("I've encountered an error: " + str(e))
+        return sendmessage("I've encountered an error: " + str(e))
 
 
 # This process is significant. Due to the infrastructure serverless sets up,
 # the response object NEEDS to be formatted in this exact manner.
 # Failure to format responses in this manner will result in the response splatting against the outgoing side of the API.
 def get_response_object(event, message="Slack message has been successfully handled.", statusCode=200):
+    print("Message got: " + message)
     body = {
         "message": message,
         "input": event
@@ -123,7 +123,7 @@ def mentions_me(text):
 # Removes the Bot's @ tag from the message for cleaner parsing.
 def strip_text(text):
     # Pulling the nasty @tag out of the message.
-    text = re.sub('<@.........>', "Wooglin,", text).strip()
+    text = re.sub('<@.........>', "", text).strip()
     return text
 
 
@@ -138,4 +138,4 @@ def sendmessage(message, blocks=None):
         'blocks': json.dumps(blocks) if blocks else None
     }).json()
 
-    return 200
+    return message
