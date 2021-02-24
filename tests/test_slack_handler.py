1815ab8cdd58ce585bd9faa251ff8c708f9fc283
cole polyak <colepolyak@gmail.com>
2021-02-24 14:53:06 -0700

    Revamped texting.

--- tests/test_slack_handler.py
+++ tests/test_slack_handler.py
@@ -1,16 +1,19 @@
 import unittest
-from source.slack_handler import slack_handler
+from testing_utilities import populate_environment
+from source.slack_handler import *
 
 
 class TestSlackHandler(unittest.TestCase):
-
     """ test the handler code """
-    # def test_hello_world(self):
-    #     event = {"event": {""}}
-    #     context = {}
-    #
-    #     resp = slack_handler(event, context)
-    #     self.assertEqual(resp["statusCode"], 200)
+    def setUp(self):
+        try:
+            test = os.environ['BOT_TOKEN']
+        except KeyError:
+            populate_environment()
+
+    def test_mentions_me_returns_proper_response(self):
+        self.assertEqual(mentions_me("Hello world"), False)
+        self.assertEqual(mentions_me("Good morning, " + os.environ['MY_ID']), True)
 
 
 if __name__ == '__main__':
--- tests/test_twilio_handler.py
+++ tests/test_twilio_handler.py
@@ -1,16 +1,15 @@
 import unittest
-from source.twilio_handler import twilio_handler
+from testing_utilities import populate_environment
+from source.slack_handler import *
 
 
 class TestTwilioHandler(unittest.TestCase):
-
     """ test the handler code """
-    def test_hello_world(self):
-        event = {}
-        context = {}
-
-        resp = twilio_handler(event, context)
-        self.assertEqual(resp["statusCode"], 200)
+    def setUp(self):
+        try:
+            test = os.environ['BOT_TOKEN']
+        except KeyError:
+            populate_environment()
 
 
 if __name__ == '__main__':
